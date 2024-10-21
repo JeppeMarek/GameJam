f365fb17959c32dd6855e02b8da19dbd5b4513f9
JeppeMarek <Jeppe_marek@hotmail.com>
2024-10-21 13:39:30 +0200

    Bouldering completed

--- GameJam/Controller.cs
+++ GameJam/Controller.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,15 +11,16 @@ namespace GameJam
     {
         private bool isAlive = Player.Lives > 0;
         private string choice;
+        private int breakfastChoice;
 
 
 
         public void FirstStage()
         {
-            
+
             while (isAlive == true)
             {
-                Console.Clear();
+                //Console.Clear();
                 Console.WriteLine("Du står op det er fredag"); // Indsæt ritig tekst senere
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine("Alarmen ringer! Du vågner i din egen seng klokken er 06.45.");
@@ -41,7 +43,7 @@ namespace GameJam
                         Console.WriteLine("Du skal vælge en mulighed for listen");
                         break;
                 }
-                
+
             }
         }
         public void Snooze()
@@ -62,7 +64,6 @@ namespace GameJam
                     Player.Lives = 0;
                     break;
                 case "3":
-                    Console.WriteLine("Du står op med det samme og dagen begynder");
                     WakeUp();
                     break;
                 default:
@@ -85,7 +86,7 @@ namespace GameJam
                     Console.WriteLine("Du glider i shampooen og slår hovedet. Alt bliver sort. DU DØD");
                     break;
                 case "2":
-                    //Breakfast();
+                    Breakfast();
                     break;
                 case "3":
                     Console.ForegroundColor = ConsoleColor.Red;
@@ -100,7 +101,126 @@ namespace GameJam
         public void Breakfast()
         {
             Console.WriteLine("Du overlevede badet. Du vil nu have noget morgenmad");
-            Console.WriteLine("");
+            Console.WriteLine("1. Drik en monner \n2. Drik en flase vand \n3. Spis en skål müsli");
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("du drikker en lækker monner");
+                    breakfastChoice = 1;
+                    Transport();
+                    break;
+                case "2":
+                    Console.WriteLine("Du drikker noget kedeligt vand");
+                    breakfastChoice = 2;
+                    Transport();
+                    break;
+                case "3":
+                    Console.WriteLine("Du spiser en skål müsli");
+                    breakfastChoice = 3;
+                    Transport();
+                    break;
+                default:
+                    Console.WriteLine("Du skal vælge en mulighed for listen");
+                    break;
+
+            }
+        }
+        public void Transport()
+        {
+            Console.WriteLine("Du har fået noget at spise eller drikke. Du skal nu afsted til skole");
+            Console.WriteLine("1. Gå i skole\n2. Cykel i skole\n3. Kør i bil til skole\n4. Tag offentlig transport");
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("Du går i skole, men bliver pludselig ramt af en lastbil. Du er død");
+                    break;
+                case "2":
+                    Console.WriteLine("Du hopper på cyklen og 1.. 2.. 3..");
+                    School();
+                    break;
+                    case "3":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Du hopper i bilen og fræser derud af, du kører over et vejbump og flyver ind i en børnehave alle dør.");
+                    break;
+                    case "4":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Du bliver blændet af solskinnet og taber din kaffe ud over buschaufføren. Han mister kontrollen og kører ned i en å. Alle dør");
+                    break;
+                    default:
+                    Console.WriteLine("Du skal vælge en mulighed for listen");
+                    break;
+            }
+        }
+        public void School()
+        {
+            Console.WriteLine("Du er på UCL");
+            Console.WriteLine("1. Tag til systemudvikling\n2. Tag til programmering\n3. Spil Slope på Y8");
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("Jan Brown myrder dig for at lave verdens grimmeste Objektmodel FYYY!!!");
+                    break;
+                    case "2":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Du programmerer en AI der dræber hele verden.");
+                    break;
+                    case "3":
+                    Console.WriteLine("Slope er for sindsygt!");
+                    WalkToBouldering();
+                    break;
+                    default:
+                    Console.WriteLine("Du skal vælge en mulighed for listen");
+                    break;
+            }
+        }
+        public void WalkToBouldering()
+        {
+            Console.WriteLine("Vælg en rute:");
+            Console.WriteLine("1. Gå under broen\n2. Gå igennem gyden");
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Det er skræmmende under broen men et par hjemløse mænd stopper ikke dig du skal jo ud og klatre");
+                    Bouldering();
+                    break;
+                    case "2":
+                    Console.WriteLine("Leif er sur, hvorfor fanden er du i Leifs gyde?! Han kaster en pokeball aka en håndgranat og du eksploderer");
+                    break;
+                    default:
+                    Console.WriteLine("Du skal vælge en mulighed for listen");
+                    break;
+            }
+        }
+        public void Bouldering()
+        {
+            Console.WriteLine("Du ankommer til bouldering hallen, Orhan venter på dig");
+            switch (breakfastChoice)
+            {
+                case 1:
+                    Console.WriteLine("Du har energi fra monneren du drak til morgenmad. Du flyver op af ruter i alle sværhedsgrader, Orhan indser at han aldrig bliver lige så god til bouldering som dig.");
+                    Party();
+                    break;
+                case 2:
+                    Console.WriteLine("Du prøver at klatre op af den nemmeste rute, men vand er for svage mennesker og giver ingen energi. Du falder ned fra væggen og lander lige akkurat udenfor madrassen under dig og du dør.");
+                    break;
+                case 3:
+                    Console.WriteLine("Dagen går relativt okay og du klatre som du normalt ville.\r\nSiden du spiste müsli til morgenmad føler du dig fyldt af energi, du går efter en sværere rute end normalt. Halvvejs oppe løber müsliens energi tør. Du falder og forstrækker en muskel, dette er lig med død for en svag IT studerende.");
+                    break;
+                default:
+                    Console.WriteLine("Du skal vælge en mulighed for listen");
+                    break;
+            }
+
+        }
+        public void Party()
+        {
+
         }
     }
 }
