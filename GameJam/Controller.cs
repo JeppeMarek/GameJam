9021a89e182e9c46d0cf1767369fdb786e6bba82
JeppeMarek <Jeppe_marek@hotmail.com>
2024-10-21 13:06:25 +0200

    Made it till breakfast

--- GameJam/Controller.cs
+++ GameJam/Controller.cs
@@ -15,9 +15,11 @@ namespace GameJam
 
         public void FirstStage()
         {
-            Console.WriteLine("Du står op det er fredag"); // Indsæt ritig tekst senere
-            while (isAlive)
+            
+            while (isAlive == true)
             {
+                Console.Clear();
+                Console.WriteLine("Du står op det er fredag"); // Indsæt ritig tekst senere
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine("Alarmen ringer! Du vågner i din egen seng klokken er 06.45.");
                 Console.WriteLine("1. Snooze alarmen i 5 min\n2. Sov igennem alarmen\n3. Stå op");
@@ -31,11 +33,9 @@ namespace GameJam
                     case "2":
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Du sover igennem alarmen og vågner aldrig igen..");
-                        Player.Lives = 0;
                         break;
                     case "3":
-                        Console.WriteLine("Du står op med det samme og dagen begynder");
-                        // WakeUp();
+                        WakeUp();
                         break;
                     default:
                         Console.WriteLine("Du skal vælge en mulighed for listen");
@@ -49,6 +49,58 @@ namespace GameJam
             Console.Clear();
             Console.WriteLine("Du har valgt at snooze");
             Console.WriteLine("Hvad vil du nu gøre?");
+            Console.WriteLine("1. Snooze alarmen i 5 min igen\n2. Sov igennem alarmen\n3. Stå op");
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Du prøver at snooze, men inden du kan trykke får du en blodprop og dør");
+                    break;
+                case "2":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Du sover igennem alarmen og vågner aldrig igen..");
+                    Player.Lives = 0;
+                    break;
+                case "3":
+                    Console.WriteLine("Du står op med det samme og dagen begynder");
+                    WakeUp();
+                    break;
+                default:
+                    Console.WriteLine("Du skal vælge en mulighed for listen");
+                    break;
+            }
+
+        }
+        public void WakeUp()
+        {
+            Console.WriteLine("Du står op med det samme og dagen begynder. Du vil nu i bad");
+            Console.WriteLine("1. Gå i bad og brug shampoo og showergel\n2.Gå i bad og brug håndsæbe\n3. Spring badet over");
+
+
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Du glider i shampooen og slår hovedet. Alt bliver sort. DU DØD");
+                    break;
+                case "2":
+                    //Breakfast();
+                    break;
+                case "3":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Du springer badet over. Naboen råber ind i din lejlighed at du lugter som et lig. Du dør af skam");
+                    break;
+                default:
+                    Console.WriteLine("Du skal vælge en mulighed for listen");
+                    break;
+
+            }
+        }
+        public void Breakfast()
+        {
+            Console.WriteLine("Du overlevede badet. Du vil nu have noget morgenmad");
+            Console.WriteLine("");
         }
     }
 }
